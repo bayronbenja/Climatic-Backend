fd0f0fe5fd47b24dd1b581d8ffcb29eb1b4dbb1e
mauricio-gdlh <mauricio.gregorio.dlh@gmail.com>
2024-11-03 00:50:07 -0300

    Update de contour_plot
    Se añaden más variables.

--- contour_plot.py
+++ contour_plot.py
@@ -10,16 +10,24 @@ import xarray
 import os
 
 
-def contour_plot(variable, dataset, shading=True):
+def contour_plot(variable, dataset, shading=True, level = None):
     """
     Grafica la temperatura sobre una imagen de fondo.
 
-    - variable: Variable a graficar. 
+    - variable: Variable a graficar.
       - Temperatura a 2 metros de la superficie: 't2m'
       - Temperatura (level): "t"
       - Temperatura en la superficie del mar: "sst"
       - Humedad específica: "q"
       - Radiación solar incidente: "tisr"
+      - Ángulo de la orografía a escala subcuadrícula: "anor"
+      - Pendiente de la orografía: "slor"
+      - Desviación estándar de la orografía: "sdor"
+      - Desviación estándar de la orografía filtrada: "sdfor"
+      - Geopotencial: "z"
+      - Geopotencial en la superficie: "z_surface"
+      - Presión media a nivel del mar: "msl"
+      - Presión en la superficie: "sp"
 
     Parameters:
     - dataset: Dataset con los datos de temperatura.
@@ -28,7 +36,26 @@ def contour_plot(variable, dataset, shading=True):
     - image_path2: Ruta de la imagen a usar para cubrir los bordes.
     - extent: Extensión geográfica de la imagen [min_lon, max_lon, min_lat, max_lat].
     - shading: Booleano para graficar el contorno o no.
+    - level: Valor de level si corresponde
     """
+
+    # Configuración de datos y títulos por variable, falta ajustar level donde corresponda.
+    variable_config = {
+        "t2m": {"label": "Unidades (°C)", "title": "Temperatura a 2 metros sobre la superficie"},
+        "t": {"label": "Unidades (°C)", "title": f"Temperatura a {level} hPa "},
+        "sst": {"label": "Unidades (°C)", "title": "Temperatura en la superficie del mar"},
+        "q": {"label": "Unidades (g/kg)", "title": "Humedad específica"},
+        "tisr": {"label": "Unidades (W/m^2)", "title": "Radiación solar incidente"},
+        "anor": {"label": "Unidades (grados)", "title": "Ángulo de la orografía a escala subcuadrícula"},
+        "slor": {"label": "Unidades (grados)", "title": "Pendiente de la orografía"},
+        "sdor": {"label": "Unidades (grados)", "title": "Desviación estándar de la orografía"},
+        "sdfor": {"label": "Unidades (grados)", "title": "Desviación estándar de la orografía filtrada"},
+        "z": {"label": "Unidades (m)", "title": "Geopotencial"},
+        "z_surface": {"label": "Unidades (m)", "title": "Geopotencial en la superficie"},
+        "msl": {"label": "Unidades (hPa)", "title": "Presión media a nivel del mar"},
+        "sp": {"label": "Unidades (hPa)", "title": "Presión en la superficie"},
+    }
+
     image_path = os.getcwd()+"/image/Mapa_REGION_border-Photoroom.png"  # Reemplaza con la ruta de tu imagen
     image_path_C = os.getcwd()+'/image/MAPA_Comunas_sexta_region.png'  # Reemplaza con la ruta de tu imagen
     image_path2 = os.getcwd()+'/image/Region_FULL_FILL.png'  # Reemplaza con la ruta de tu imagen
@@ -37,12 +64,8 @@ def contour_plot(variable, dataset, shading=True):
     # Extraer datos del dataset
     temperature = dataset.values - 273.15  # Kelvin a Celsius
 
-    if variable == "t2m" or variable == "t" or "sst":
-      datos = dataset.values - 273.15  # Kelvin a Celsius
-    elif variable == "q": # Por si se quisiera hacer un arreglo de unidad de medida
-      datos = dataset.values
-    elif variable == "tisr": # Por si se quisiera hacer un arreglo de unidad de medida
-      datos = dataset.values
+    # Extraer datos y convertir unidades si es necesario
+    datos = dataset.values - 273.15 if variable in ("t2m", "t", "sst") else dataset.values
 
     lats = dataset['latitude'].values
     lons = dataset['longitude'].values
@@ -65,60 +88,39 @@ def contour_plot(variable, dataset, shading=True):
 
     # Graficar los datos
     if shading:
+      if variable in ("z", "z_surface", "msl", "sp"): #Simula isobaras
+        cont = ax.contourf(
+            lons, lats, datos, cmap='RdYlBu_r', transform=ccrs.PlateCarree(), levels=15,
+            zorder=1
+        )
+        ax.clabel(cont, inline=1, fontsize=10, fmt=' {:.0f} '.format, colors='black')
+      else:
         cont = ax.contourf(
             lons, lats, datos, cmap='RdYlBu_r', transform=ccrs.PlateCarree(), levels=15,
             zorder=1
         )
     else:
-      img = mpimg.imread(image_path_C)
-      ax.imshow(img, origin='upper', extent=extent, transform=ccrs.PlateCarree(), zorder=3)
-      cont = ax.contour(
-          lons, lats, datos, cmap='RdYlBu_r', transform=ccrs.PlateCarree(), levels=20,
-          zorder=4)
-      ax.clabel(cont,
-            inline=1,
-            fontsize=8,
-            fmt=' {:.0f} '.format,  # Labes as integers, with some extra space.
-      # Cut the line where the label will be placed.
-        )
+        img = mpimg.imread(image_path_C)
+        ax.imshow(img, origin='upper', extent=extent, transform=ccrs.PlateCarree(), zorder=3)
+        cont = ax.contour(
+            lons, lats, datos, cmap='RdYlBu_r', transform=ccrs.PlateCarree(), levels=20,
+            zorder=4)
+        ax.clabel(cont, inline=1, fontsize=10, fmt=' {:.0f} '.format)
     
-    # Configurar el colorbar,  para que tenga más valores y se ajuste a la variable
-    if variable == "t2m":
-      cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label='Unidades (°C)')
-      cbar.locator = mticker.MaxNLocator(nbins=20)  # Cambia el número de etiquetas
-      cbar.update_ticks()  # Actualiza las etiquetas en el colorbar
-      plt.title(f'Temperatura a 2 metros sobre la superficie\n{date} - {date_h}', size=10, weight='bold')
-
-    elif variable == "t":
-      cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label='Unidades (°C)')
-      cbar.locator = mticker.MaxNLocator(nbins=20)  # Cambia el número de etiquetas
-      cbar.update_ticks()  # Actualiza las etiquetas en el colorbar
-      plt.title(f'Temperatura (level)\n{date} - {date_h}', size=10, weight='bold')
-
-    elif variable == "sst":
-      cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label='Unidades (°C)')
-      cbar.locator = mticker.MaxNLocator(nbins=20)  # Cambia el número de etiquetas
-      cbar.update_ticks()  # Actualiza las etiquetas en el colorbar
-      plt.title(f'Temperatura en la superficie del mar\n{date} - {date_h}', size=10, weight='bold')
-
-    elif variable == "q":
-      cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label='Unidades (g/kg) (gramos por kilogramo)')
-      cbar.locator = mticker.MaxNLocator(nbins=20)  # Cambia el número de etiquetas
-      cbar.update_ticks()  # Actualiza las etiquetas en el colorbar
-      plt.title(f'Humedad específica\n{date} - {date_h}', size=10, weight='bold')
-      
-    elif variable == "tisr":
-      cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label='Unidades (W/m^2) (vatios por metro cuadrado)')
-      cbar.locator = mticker.MaxNLocator(nbins=20)  # Cambia el número de etiquetas
-      cbar.update_ticks()  # Actualiza las etiquetas en el colorbar
-      plt.title(f'Radiación solar incidente\n{date} - {date_h}', size=10, weight='bold')
+    # Configurar el colorbar y el título
+    if variable in variable_config:
+        config = variable_config[variable]
+        cbar = plt.colorbar(cont, orientation='vertical', pad=0.15, fraction=0.03, label=config["label"])
+        cbar.locator = mticker.MaxNLocator(nbins=20)
+        cbar.update_ticks()
+        plt.title(f'{config["title"]}\n{date} - {date_h}', size=10, weight='bold')
 
     # Añadir líneas de la grilla
     bar = ax.gridlines(draw_labels=True, linewidth=1, color='black', alpha=0.8)
     bar.xlocator = mticker.FixedLocator(lons)
     bar.ylocator = mticker.FixedLocator(lats)
 
-    # Mostrar etiquetas solo en el borde superior y izquierdo
+    # Mostrar etiquetas solo en el borde superior e izquierdo
     bar.right_labels = False
     bar.top_labels = False
 
